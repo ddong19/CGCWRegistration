649345ce5676d85798f7d7e5efe91894943da5b4
ddong19 <dandong@umich.edu>
2024-04-24 14:43:31 -0400

    Form correctly resetting!
    Registration form is correctly resetting upon submission.
    
    Bug was because the original controller return a Json if it detected it was an AJAX request and a view if it wasn't. But the if statement wasn't working so it only returned a view and never entered our form reset function.

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -28,41 +28,22 @@ namespace CGCWRegistration.Controllers
             return View(viewModel);
         }
 
+        // POST
         [HttpPost]
         public async Task<ActionResult> Register(UserRegistrationViewModel model)
         {
             if (!ModelState.IsValid)
             {
-                if (Request.IsAjaxRequest())
-                {
-                    return Json(new { success = false, message = "Invalid input data" });
-                }
-                // Prepare the same ViewModel with validation messages for non-ajax request
-                return View(await _registrationService.PrepareRegistrationViewModelAsync());
+                return Json(new { success = false, message = "Invalid input data" });
             }
-
             try
             {
                 await _registrationService.RegisterUserAsync(model);
-                if (Request.IsAjaxRequest())
-                {
-                    return Json(new { success = true, message = "Registration successful!" });
-                }
-
-                            // Clear ModelState to remove old input data
-            ModelState.Clear();
-            // Prepare a fresh ViewModel
-            var newViewModel = await _registrationService.PrepareRegistrationViewModelAsync();
-            return View("Register", newViewModel);
+                return Json(new { success = true, message = "Registration successful!" });
             }
             catch (Exception ex)
             {
-                if (Request.IsAjaxRequest())
-                {
-                    return Json(new { success = false, message = ex.Message });
-                }
-                // Handle error scenario for non-ajax request, perhaps display an error view
-                return View("Error");
+                return Json(new { success = false, message = ex.Message });
             }
         }
     }
